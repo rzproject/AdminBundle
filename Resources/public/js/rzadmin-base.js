fe8cec1efafba0a00e4f4506c68e7189b3d36060
mellzamora <mellzamora@outlook.com>
2015-01-29 12:25:01 +0800

    fix sonata_type_native_collection js and template

--- Resources/public/js/rzadmin-base.js
+++ Resources/public/js/rzadmin-base.js
@@ -153,8 +153,9 @@ var rzadmin = {
         if (typeof rzadmin_fieldtype !== 'undefined') {
             rzadmin_fieldtype.init();
         }
-
     },
+
+
 
     /**
      * render log message
@@ -1276,19 +1277,25 @@ var rzadmin = {
 
             var container = jQuery(this).closest('[data-prototype]');
             var proto = container.attr('data-prototype');
+            var protoName = container.attr('data-prototype-name') || '__name__';
             // Set field id
             //var idRegexp = new RegExp(container.attr('id')+'___name__','g');
             var idRegexp = new RegExp(container.attr('id')+'_'+protoName,'g');
             proto = proto.replace(idRegexp, container.attr('id')+'_'+(container.children().length - 1));
 
             // Set field name
+
             var parts = container.attr('id').split('_');
-//            var nameRegexp = new RegExp(parts[parts.length-1]+'\\]\\[__name__','g');
             var nameRegexp = new RegExp(parts[parts.length-1]+'\\]\\['+protoName,'g');
             proto = proto.replace(nameRegexp, parts[parts.length-1]+']['+(container.children().length - 1));
-            jQuery(proto).insertBefore(jQuery(this).parent());
+
+            jQuery(proto)
+                .insertAfter(jQuery('.sonata-collection-container-'+container.attr('id')))
+                .trigger('sonata-admin-append-form-element')
+            ;
 
             jQuery(this).trigger('sonata-collection-item-added');
+
         });
 
         jQuery(subject).on('click', '.sonata-collection-delete', function(event) {
