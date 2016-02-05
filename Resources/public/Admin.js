9bde166504745b452d53a8b07922625a3f7a2055
mellzamora <mellzamora@outlook.com>
2016-02-05 16:39:29 +0800

    added blockUI spinner for ajax loading

--- Resources/public/Admin.js
+++ Resources/public/Admin.js
@@ -18,6 +18,25 @@ jQuery(document).ready(function() {
     Admin.setup_per_page_switcher(document);
     Admin.setup_collection_buttons(document);
     Admin.shared_setup(document);
+
+    jQuery(document).ajaxStart(function() {
+        jQuery.blockUI({ message: '<div class="load-container"><div class="loader">Loading...</div></div>',
+                         css: {
+                            border: 'none',
+                            padding: '0px',
+                            width:   'auto',
+                            left:     '50%',
+                            backgroundColor: 'transparent',
+                            '-webkit-border-radius': '10px',
+                            '-moz-border-radius': '10px',
+                            opacity:.4,
+                            color: '#fff'
+                         }});
+    });
+
+    jQuery(document).ajaxComplete(function(){
+        jQuery.unblockUI();
+    });
 });
 
 jQuery(document).on('sonata-admin-append-form-element', function(e) {
@@ -54,6 +73,33 @@ var Admin = {
         Admin.setup_rpage(subject);
         Admin.setup_list_modal(subject);
         Admin.setup_tabdrop(subject);
+    },
+
+    apped_spinner: function() {
+        var opts = {
+            lines: 17 // The number of lines to draw
+            , length: 28 // The length of each line
+            , width: 52 // The line thickness
+            , radius: 10 // The radius of the inner circle
+            , scale: 2 // Scales overall size of the spinner
+            , corners: 1 // Corner roundness (0..1)
+            , color: '#000' // #rgb or #rrggbb or array of colors
+            , opacity: 0.25 // Opacity of the lines
+            , rotate: 0 // The rotation offset
+            , direction: 1 // 1: clockwise, -1: counterclockwise
+            , speed: 1 // Rounds per second
+            , trail: 100 // Afterglow percentage
+            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+            , zIndex: 2e9 // The z-index (defaults to 2000000000)
+            , className: 'spinner' // The CSS class to assign to the spinner
+            , top: '50%' // Top position relative to parent
+            , left: '50%' // Left position relative to parent
+            , shadow: true // Whether to render a shadow
+            , hwaccel: false // Whether to use hardware acceleration
+            , position: 'absolute' // Element positioning
+        }
+
+        return new Spinner(opts).spin();
     },
 
     setup_fileinput: function(subject) {
